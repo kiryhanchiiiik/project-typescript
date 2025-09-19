96647c47419eac8648104e645be5c46aff976bb8
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-19 10:21:37 +0300

    Initials commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { store } from './store';
-import { Provider } from 'react-redux';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </Provider>
-);
+  </StrictMode>,
+)
