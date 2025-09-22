d1d77feb24978d030e39b6f58aff9f25276bbf87
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-22 19:33:07 +0300

    Navbar Component - Setup

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,11 +1,11 @@
-import { Header } from "@/components";
+import { Header, NavBar } from "@/components";
 import { Outlet } from "react-router-dom";
 
 function HomeLayout() {
   return (
     <>
       <Header />
-      <nav>navbar</nav>
+      <NavBar />
 
       <div className="align-element py-20">
         <Outlet />
