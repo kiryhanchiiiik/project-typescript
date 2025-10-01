7fe6b450b7b109c96b54afa91150fec4fe4dafbe
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-10-01 10:27:07 +0300

    Products List Component

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,5 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
+import { useState } from "react";
+import { LayoutGrid, List } from "lucide-react";
+import { ProductsResponse } from "@/utils";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+
 function ProductsContainer() {
-  return <div>ProductsContainer</div>;
+  return (
+    <div>
+      <ProductsList />
+    </div>
+  );
 }
 
 export default ProductsContainer;
