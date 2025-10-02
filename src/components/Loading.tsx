a2a6e0cae4a4f799e4e1eb9b55d016de7b72f251
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-10-02 12:12:47 +0300

    Loading Component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,20 @@
-function Loading() {
-  return <div className="text-lg">Loading...</div>;
-}
+import { Skeleton } from "./ui/skeleton";
 
+function Loading() {
+  return (
+    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 ">
+      {Array.from({ length: 3 }).map((_, index) => {
+        return (
+          <div key={index} className="flex flex-col space-y-3">
+            <Skeleton className="h-[125px] w-full rounded-xl" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 mx-auto w-[250px]" />
+              <Skeleton className="h-4 mx-auto w-[200px]" />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 export default Loading;
