5c040dc3753a8f5b0785c71068e69057288a265e
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-10-12 11:11:39 +0300

    Toast Component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import { store } from "./store.ts";
+import { Toaster } from "@/components/ui/toaster";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
+    <Toaster />
     <App />
   </Provider>
 );
