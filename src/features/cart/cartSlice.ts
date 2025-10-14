8035d88129afc98dbf6c14eb4461b1ee5f8d58e3
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-10-14 09:36:05 +0300

    Cart Slice - Setup

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,12 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { CartState } from "@/utils";
 
-const initialState = {
-  name: "cart slice",
+const defaultState: CartState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
 };
+
+const getCartFromLocalStorage = (): CartState => {
+  const cart = localStorage.getItem("cart");
+  return cart ? JSON.parse(cart) : defaultState;
+};
+
 const cartSlice = createSlice({
   name: "cart",
-  initialState,
-  reducers: {},
+  initialState: getCartFromLocalStorage(),
+  reducers: {
+    addItem: () => {},
+    clearCart: () => {},
+    removeItem: () => {},
+    editItem: () => {},
+    calculateTotals: () => {},
+  },
 });
+
+export const { addItem, clearCart, removeItem, editItem, calculateTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
